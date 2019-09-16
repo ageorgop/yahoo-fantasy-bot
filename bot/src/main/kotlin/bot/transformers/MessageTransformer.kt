c57b5d8f757b27a61f62f6e9a6754bfcdcc60e4c
Alex Georgopoulos <ageorgop@housecanary.com>
2019-09-16 11:33:34 -0700

    Take three trying to silence the scoring

--- bot/src/main/kotlin/bot/transformers/MessageTransformer.kt
+++ bot/src/main/kotlin/bot/transformers/MessageTransformer.kt
@@ -12,6 +12,8 @@ fun Observable<Message>.convertToStringMessage(): Observable<String> =
             is Message.Transaction.Trade -> createMessage("===TRADE ALERT===", it)
             is Message.Transaction.Commish -> createMessage("===COMMISH ALERT===", it)
             is Message.Standings -> createMessage("===STANDINGS ALERT===", it)
+            is Message.Score -> createMessage(null, it)
+            is Message.CloseScore -> createMessage(null, it)
             is Message.MatchUp -> createMessage("===MATCH UP ALERT===", it)
             is Message.Generic -> createMessage(null, it)
             is Message.Unknown -> createMessage(null, it)
